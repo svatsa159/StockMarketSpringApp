e86feb5f30a9da8f9af16c85ea956c95476aabb6
svatsa159 <svatsa159@gmail.com>
2020-08-27 16:53:52 +0530

    Swagger Implementation

--- src/main/java/com/stockmarket/StockMarketApplication.java
+++ src/main/java/com/stockmarket/StockMarketApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class StockMarketApplication {
 
 	public static void main(String[] args) {
@@ -17,4 +24,9 @@ public class StockMarketApplication {
 		return new ModelMapper();
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 }
